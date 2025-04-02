d2bef374a0b78334445bd53a024acdac27f11aaf
Gustavo Silva <gsilvaaragao@gmail.com>
2025-04-02 14:55:30 -0300

    Ajuste no Log do express

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ app.use(bodyParser.json())
 app.use("/products", ProductRoutes)
 
 app.listen(3000, () => {
-  console.log(`Server running on port 3000`)
+  console.log("Servidor rodando na porta: 3000")
 })
 
 export default app
